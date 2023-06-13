94b0200c204332362a60d5cee70b280564881dbc
nzh2534 <91237859+nzh2534@users.noreply.github.com>
2023-06-13 15:12:00 -0400

    intall dotenv

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
