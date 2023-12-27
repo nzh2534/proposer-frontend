7f86f0422cfd03120f9f4a5576852b23c0a5130d
Aaron Dunmore <aedunmore@gmail.com>
2023-12-27 14:44:19 -0500

    Improved style of ListView component

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,4 +1,5 @@
 import ListGroup from "react-bootstrap/ListGroup";
+import "./ListView.css";
 
 function Proposal(props) {
   const { proposal } = props;
@@ -11,11 +12,17 @@ function Proposal(props) {
 
 function ListView({ proposals }) {
   return (
-    <ListGroup defaultActiveKey="#">
-      {proposals?.map((item, index) => {
-        return <Proposal proposal={item} key={index} />;
-      })}
-    </ListGroup>
+    <div className="list-view-container">
+      <div className="list-view">
+        Select a proposal to view
+        <hr color="black" style={{ width: "11em" }} />
+        <ListGroup defaultActiveKey="#">
+          {proposals?.map((item, index) => {
+            return <Proposal proposal={item} key={index} />;
+          })}
+        </ListGroup>
+      </div>
+    </div>
   );
 }
 
